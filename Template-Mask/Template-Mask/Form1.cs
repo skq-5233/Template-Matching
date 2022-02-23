837e5ff8a95e3f4a8be4f9745ab446ab35422072
C#EXAMPLE <794919561@qq.com>
2022-02-23 16:20:47 +0800

    模板-掩膜-0223

---
+++ Template-Mask/Template-Mask/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu;
+using Emgu.CV;
+using Emgu.Util;
+using System.Runtime.Serialization;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.CV.Cuda;
+using System.IO;
+using NPOI;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using NPOI.HSSF.UserModel;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Template_Mask
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
